43fbb0f89cbed6a5000ef981c3bc104d29506f4e
Shashank <shashank.kanaujia.1@gmail.com>
2019-04-20 02:26:06 +1000

    Moved to class

--- Window.py
+++ Window.py
@@ -1,9 +1,14 @@
 import wx
 
+class Example(wx.Frame):
+    def __init__(self, parent, title):
+        super(Example, self).__init__(parent, title=title, size=(300, 200))
+        self.Center()
+
 def main():
     app = wx.App()
-    frame = wx.Frame(None, title='Simple application')
-    frame.Show()
+    example = Example(None, title='Example window')
+    example.Show()
     app.MainLoop()
 
 if __name__ == '__main__':
